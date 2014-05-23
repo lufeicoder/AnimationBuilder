0c0a0fae7e8db26c0fe2896d56a2b153fd70599c
lufeicoder <lufei.han@mingdao.com>
2014-05-23 15:31:19 +0800

    add projects into my repository
    start animation

---
+++ AnimationBuilder/AnimationBuilder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimationBuilder
+//
+//  Created by WeeTom on 14-5-23.
+//  Copyright (c) 2014年 ___migndao___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnimationBuilder/AnimationBuilder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnimationBuilder
+//
+//  Created by WeeTom on 14-5-23.
+//  Copyright (c) 2014年 ___migndao___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
