46ec0c1ad44282df3ae0076266eb0865cb4a9c1a
lufeicoder <lufei.han@mingdao.com>
2014-05-23 18:27:54 +0800

    update

--- AnimationBuilder/AnimationBuilder/main.m
+++ AnimationBuilder/AnimationBuilder/main.m
@@ -10,9 +10,28 @@
 
 #import "AppDelegate.h"
 
+void outdate(void)
+{
+    NSDate *date = [NSDate date];
+    NSTimeZone *zone = [NSTimeZone systemTimeZone];
+    NSInteger interval = [zone secondsFromGMTForDate: date];
+    NSDate *localeDate = [date  dateByAddingTimeInterval: interval];
+    NSLog(@"enddate=%@",localeDate);
+    
+    NSTimeInterval perdaySeconds = 60*24*60;
+    date = [NSDate dateWithTimeIntervalSinceNow:perdaySeconds];
+    NSLog(@"data:%@",date);
+    
+    NSDateComponents *com = [[NSCalendar currentCalendar] components:NSCalendarCalendarUnit fromDate:date];
+    NSLog(@"com%@",[[NSCalendar currentCalendar] dateFromComponents:com]);
+}
+
 int main(int argc, char * argv[])
 {
+    outdate();
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
+    
 }
+
